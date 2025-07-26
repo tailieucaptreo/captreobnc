28899d7f4ee123e0b82ab5cd981bca0775f3cbbe
tailieucaptreo <levantrong011091@gmail.com>
2025-07-26 11:39:42 +0700

    🔧 Sửa lỗi 500 trang login bằng redirect Google

--- tailieucaptreo/urls.py
+++ tailieucaptreo/urls.py
@@ -3,13 +3,17 @@ from django.urls import path, include
 from django.contrib.auth.views import LogoutView, LoginView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def google_login(request):
+    return redirect('social:begin', backend='google-oauth2')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # ✅ CHỈ CẦN 1 LẦN
     path('oauth/', include('social_django.urls', namespace='social')),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', google_login, name='login'),  # ✅ Thay vì LoginView
     path('ckeditor/', include('ckeditor_uploader.urls')),  # ✅ CKEditor upload
 ]
 
