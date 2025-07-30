26ff17595c9b6505510cef69807704e8af3add0c
tailieucaptreo <levantrong011091@gmail.com>
2025-07-30 13:34:37 +0700

    cap nhat lai settings.py

--- tailieucaptreo/settings.py
+++ tailieucaptreo/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv 
 load_dotenv()
 
@@ -57,10 +58,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'tailieucaptreo.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = []
